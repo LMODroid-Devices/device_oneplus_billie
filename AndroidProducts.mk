4097c017e10347eea495dbe5b58f949a671cdab5
LMO GerritBot <gerrit@libremobileos.com>
2024-08-23 05:43:34 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_billie.mk
+    $(LOCAL_DIR)/lmodroid_billie.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_billie-userdebug
+    lmodroid_billie-userdebug
